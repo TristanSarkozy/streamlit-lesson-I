57138f88f07ce1484b55b7bf8ca2254c93cb43bc
Suciu Dan <suciu_t_dan@yahoo.com>
2024-04-19 13:24:36 +0000

    Add to calculator_page beta columns function to define four options to calculate, the logic, raise an error

--- 2_app_with_multiple_pages.py
+++ 2_app_with_multiple_pages.py
@@ -4,7 +4,7 @@ from app_pages.multi_page import Multipage
 from app_pages.page1 import page1_body
 from app_pages.page2 import page2_body
 
-app = Multipage(app_name= "This is my first multi-page Streamlit App")
+app = Multipage(app_name = "This is my first multi-page Streamlit App")
 
 app.app_page("Page 1", page1_body)
 app.app_page("Page 2", page2_body)
