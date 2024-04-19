f8f437bb0c17a9f1f07f4d5f1d4adc58ce3560c6
Suciu Dan <suciu_t_dan@yahoo.com>
2024-04-19 13:28:59 +0000

    correct typo

--- app_pages/page_calculator.py
+++ app_pages/page_calculator.py
@@ -6,12 +6,12 @@ def calculator_body():
     with col1:
         num1 = st.number_input(label = 'Enter the first integer', step = 1)
     with col2:
-        num1 = st.number_input(label = 'Enter the second integer', step = 2)
+        num2 = st.number_input(label = 'Enter the second integer', step = 2)
     with col3:
-        operator = st.selectbox(label 'Select an operator',
+        operator = st.selectbox(label = 'Select an operator',
                                 options = ['Add', 'Subtract', 'Multiply', 'Divide'])
     if st.button('Click here to calculate'):
-        if num2 == 0 and operator =='Divide':
+        if num2 == 0 and operator == 'Divide':
             st.error('Division by zero error. Enter a non-zero integer.')
         else:
             calculator_function(num1, num2, operator)
