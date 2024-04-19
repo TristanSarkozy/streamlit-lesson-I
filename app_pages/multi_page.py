bc51dc5eb5fb7de4e3cb453ac8b425097dfd6ae3
Suciu Dan <suciu_t_dan@yahoo.com>
2024-04-19 09:53:44 +0000

    Add a Python class to generate a multi-page object

--- app_pages/multi_page.py
+++ app_pages/multi_page.py
@@ -1,1 +1,21 @@
+import streamlit as st
+
+class Multipage:
+
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+
+        st.set_page_config(
+            page_title = self.app_name,
+            page_icon = ":random:"
+        )
+    
+    def app_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio('Menu', self.pages, format_func=lambda page['title'])
+        page['function']()
 
